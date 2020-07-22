ddc8966420cf835b46c469bae0aad8dd7c9e4a2a
Jose Ignacio Vaqueiro Gonzalez <ignaciovaqdevelop@hotmail.com>
2020-07-22 11:30:01 -0500

    footer fixed

--- JS/login.js
+++ JS/login.js
@@ -1,9 +1,11 @@
+
+
 function loginForm(){
     validate();
 }
 
 function validate(){
-    /*INPUTS ELEMENTS*/
+        /*INPUTS ELEMENTS*/
     var firstName_Textbox = document.getElementById('txtFirstName');
     var lastName_Textbox = document.getElementById('txtLastName');
     var email_Textbox = document.getElementById('txtEmail');
@@ -26,51 +28,65 @@ function validate(){
     var lastName_error = document.getElementById('last_error');
     var email_error = document.getElementById('email_error');
     var pass_error = document.getElementById('pass_error');
-
     
     if(firstName === '') {
         firstName_error.style.display = "block";
         firstName_Textbox.style.border = "1.5px solid red";
         firstName_Textbox.style.borderRadius = "3px";
         first_icon_error.style.display = "block";
+        
+        firstName_Textbox.placeholder = "";
 	} else {
         firstName_error.style.display = "none";
         firstName_Textbox.style.border = "1px solid green";
         firstName_Textbox.style.borderRadius = "3px";
         first_icon_error.style.display = "none";
 	}
+    
     if(lastName === '') {
 		lastName_error.style.display = "block";
         lastName_Textbox.style.border = "1.5px solid red";
         lastName_Textbox.style.borderRadius = "3px";
         last_icon_error.style.display = "block";
+        
+        lastName_Textbox.placeholder = "";
 	} else {
         lastName_error.style.display = "none";
         lastName_Textbox.style.border = "1px solid green";
         lastName_Textbox.style.borderRadius = "3px";
         last_icon_error.style.display = "none";
 	}
+    
     if(email === '') {
 		email_error.style.display = "block";
         email_Textbox.style.border = "1.5px solid red";
         email_Textbox.style.borderRadius = "3px";
         email_icon_error.style.display = "block";
+        
+        email_Textbox.placeholder = "email@example/com";
+        
 	}else if (!isEmail(email)) {
 		email_error.style.display = "block";
         email_Textbox.style.border = "1.5px solid red";
         email_Textbox.style.borderRadius = "3px";
         email_icon_error.style.display = "block";
+        
+        email_Textbox.placeholder = "email@example/com";
+        
+        
 	} else {
         email_error.style.display = "none";
         email_Textbox.style.border = "1px solid green";
         email_Textbox.style.borderRadius = "3px";
         email_icon_error.style.display = "none";
 	}
+    
     if(password === '') {
 		pass_error.style.display = "block";
         password_Textbox.style.border = "1.5px solid red";
         password_Textbox.style.borderRadius = "3px";
         pass_icon_error.style.display = "block";
+        password_Textbox.placeholder = "";
 	} else {
         pass_error.style.display = "none";
         password_Textbox.style.border = "1px solid green";
@@ -79,7 +95,6 @@ function validate(){
 	}
    
 }
-
 
 function isEmail(email) {
 	return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
